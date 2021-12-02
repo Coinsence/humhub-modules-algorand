1f68c360f1aaac69aa55107d6173063b774d46de
gdalyy <daly.ghaith@gmail.com>
2021-12-03 00:08:21 +0100

    adding alogrand module config file

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @link https://coinsence.org/
+ * @copyright Copyright (c) 2018 Coinsence
+ * @license https://www.humhub.com/licences
+ *
+ * @author Daly Ghaith <daly.ghaith@gmail.com>
+ */
+
+return [
+    'id' => 'algorand',
+    'class' => 'humhub\modules\algorand\Module',
+    'namespace' => 'humhub\modules\algorand',
+];
+
